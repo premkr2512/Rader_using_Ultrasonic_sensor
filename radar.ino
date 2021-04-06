ff22c31132232af5272376591b348278bfafc944
Tk_dude <74595868+premkr2512@users.noreply.github.com>
2021-04-06 12:39:04 +0530

    Update radar.ino

--- radar.ino
+++ radar.ino
@@ -1,1 +1,9 @@
-
+#include "ESP8266WiFi.h"
+#include "Adafruit_MQTT.h"
+#include "Adafruit_MQTT_Client.h"
+#define WLAN_SSID       "AndroidAP"
+#define WLAN_PASS       "098765432111"
+#define AIO_SERVER      "io.adafruit.com"
+#define AIO_SERVERPORT  1883                   // use 8883 for SSL
+#define AIO_USERNAME    "Prem2512"
+#define AIO_KEY         "038bdc81de394b85abbf082221532aef"
