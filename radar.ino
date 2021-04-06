e82d19a4ccf38a4773a308105b31a4bd12225a86
Tk_dude <74595868+premkr2512@users.noreply.github.com>
2021-04-06 12:42:26 +0530

    all set up are done

--- radar.ino
+++ radar.ino
@@ -4,6 +4,32 @@
 #define WLAN_SSID       "AndroidAP"
 #define WLAN_PASS       "098765432111"
 #define AIO_SERVER      "io.adafruit.com"
-#define AIO_SERVERPORT  1883                   // use 8883 for SSL
+#define AIO_SERVERPORT  1883                  
 #define AIO_USERNAME    "Prem2512"
 #define AIO_KEY         "038bdc81de394b85abbf082221532aef"
+WiFiClient client;
+
+Adafruit_MQTT_Client mqtt(&client, AIO_SERVER, AIO_SERVERPORT, AIO_USERNAME, AIO_KEY);
+
+
+Adafruit_MQTT_Publish data = Adafruit_MQTT_Publish(&mqtt, AIO_USERNAME "/feeds/TXT");
+Adafruit_MQTT_Publish data1 = Adafruit_MQTT_Publish(&mqtt, AIO_USERNAME "/feeds/TXT");
+
+
+void MQTT_connect();
+#include <Servo.h>. 
+#include<LiquidCrystal.h>
+LiquidCrystal lcd(D3,D4,D5,D6,D7,D8); 
+#define trigPin = D0;
+#define echoPin = D1;
+
+long duration;
+int distance;
+Servo myServo; 
+
+pinMode(trigPin, OUTPUT);
+
+pinMode(echoPin, INPUT); 
+
+Serial.begin(9600);
+myServo.attach(D2); 
